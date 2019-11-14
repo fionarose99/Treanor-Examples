e5ea9bebe97481f1a5429292e5f2c1df47c0af6a
Mike Treanor <micitari@gmail.com>
2019-11-14 16:47:56 -0500

    Begining of platformer final rpoject

---
+++ examples/Platformer/Assets/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+	public Animator doubleJumpButtonAnimator;
+
+	int acorns = 100;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	public void shopButtonClicked()
+	{
+		if (acorns < 200) {
+			doubleJumpButtonAnimator.SetTrigger("denied");
+		}
+	}
+}
